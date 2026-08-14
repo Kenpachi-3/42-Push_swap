af158a5a9da306a539e904a114b23f3fd0123b18
fpresti96 <presti.francesco96@gmail.com>
2026-08-14 18:43:15 +0200

    Create ft_disorder_metric.c

---
+++ ft_disorder_metric.c
@@ -0,0 +1,8 @@
+float disorder_metric(s_stack a)
+{
+    //calculate disorder: subject suggests
+    //every pair where bigger before smaller -> +1
+    //then disorder = disordered_pairs / total_pairs
+    //return disorder
+    return ( /* float that expresses disorder*/ );
+}
