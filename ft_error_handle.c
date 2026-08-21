82df3c2a15f1492032e0fe8d8b1e2efab0fc3696
fpresti96 <presti.francesco96@gmail.com>
2026-08-21 22:00:16 +0200

    commit

---
+++ ft_error_handle.c
@@ -0,0 +1,8 @@
+void    ft_error_handle(/*something*/)
+{
+    if(/*something*/)
+    {
+        printf(/*something*/);
+        exit(1);//check the 1
+    }
+}
--- ft_make_stack.c
+++ ft_make_stack.c
@@ -12,16 +12,43 @@
 
 #include "libft.h"
 
-static void	ft_make_stack(s_stack stack, char *arr){	//arr is the array taken from the argv, with numbers in char, to be converted to int
-	int	*tmp;
+static int	ft_digit_count(int n)
+{
+	int count;
+
+	if (n != 0)
+	{
+		if (n < 0)
+		{
+			count++;
+			n *= -1;
+		}
+		while (n > 0)
+		{
+			count++;
+			n /= 10;
+		}
+		return (count);
+	}
+	return (1);
+}
+
+static void	ft_make_stack(s_stack stack, char *arr)
+{	//arr is the array taken from the argv, with numbers in char, to be converted to int
+	char	*tmp_arr;
+	int	*tmp_int;
+	int	num;
 	
-	*stack = ft_lstnew(ft_atoi(arr));
-	arr++;//MOVE FORWARD a number of steps equal to the number of digits of the input number
+	tmp_arr = arr;
+	num = ft_atoi(tmp_arr);
+	*stack = ft_lstnew(num);
+	num = ft_digit_count(n);
+	arr += num;//MOVE FORWARD a number of steps equal to the number of digits of the input number
 	while (arr)
 	{
-		tmp = ft_lstnew(atoi(arr));
-		ft_lstadd_back(stack, tmp);
+		tmp_int = ft_lstnew(atoi(tmp_arr));
+		ft_lstadd_back(stack, tmp_int);//fix num = digit add to arr
 		//these two could be in just one line but perhaps this is clearer
-		arr++;
+		tmp_arr++;
 	}
 }
